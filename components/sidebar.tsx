8e5030f6ffd3f96929289fb0bb2a6b9bca9f896d
Hanafe Mira <miraahanafee@gmail.com>
2024-12-03 13:24:24 +0530

    INITIAL

---
+++ components/sidebar.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { CoinsIcon, HomeIcon, Layers2Icon, ShieldCheckIcon } from 'lucide-react';
+import React from 'react'
+import Logo from './logo';
+import { buttonVariants } from './ui/button';
+import Link from 'next/link';
+
+const routes = [
+    {
+        href: "",
+        label: "Home",
+        icon: HomeIcon,
+
+    },
+    {
+        href: "workflows",
+        label: "Workflows",
+        icon: Layers2Icon,
+
+    },
+    {
+        href: "credentials",
+        label: "Credentials",
+        icon: ShieldCheckIcon,
+
+    },
+    {
+        href: "billing",
+        label: "Billing",
+        icon: CoinsIcon,
+
+    },
+    
+];
+function DesktopSidebar() {
+  return (
+    <div className="hidden relative md:block min-w-[280px] max-w-[280px] h-screen overflow-hidden w-full 
+    bg-primary/5 dark:bg-seondary/30 dark:text-foreground text-muted-foreground border-r-2 
+    border-separate">
+        <div className="flex items-center justify-center gap-2 border-b-[1px] border-separate p-4">
+            <Logo />
+        </div>
+        <div className="flex flex-col p-2">
+            {routes.map(route => (
+                <Link key={route.href} href={route.href}
+                className={
+                    buttonVariants({})
+                }>
+                    <route.icon size={20} />
+                    {route.label}
+                </Link>
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default DesktopSidebar
