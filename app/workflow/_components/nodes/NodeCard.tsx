888f8a70108ad62ddeeef3ce627058d11340005e
Hanafe Mira <miraahanafee@gmail.com>
2024-12-26 11:35:08 +0530

    center node on double click

--- app/workflow/_components/nodes/NodeCard.tsx
+++ app/workflow/_components/nodes/NodeCard.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { cn } from '@/lib/utils';
+import { useReactFlow } from '@xyflow/react';
 import React, { ReactNode } from 'react'
 
 function NodeCard({
@@ -12,8 +13,26 @@ function NodeCard({
     nodeId: string;
     isSelected: boolean;
 }) {
+    const { getNode, setCenter } = useReactFlow();
   return (
-    <div className={cn(
+    <div 
+    onDoubleClick={
+        () => {
+            const node = getNode(nodeId);
+            if (!node) return;
+            const { position, measured } = node;
+            if(!position || !measured) return;
+            const { width, height } = measured;
+            const { x, y } = position;
+            console.log("@POSITION", position);
+            if(x===undefined || y===undefined) return;
+            setCenter(x,y, {
+                zoom: 1,
+                duration: 500,
+            })
+        }
+    }
+    className={cn(
         "rounded-md cursor-pointer bg-background border-2 border-separate w-[420px] text-xs flex flex-col gap-1",
         isSelected && 'border-primary')}>
             {children}
