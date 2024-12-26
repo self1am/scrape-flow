e3a2931220a703a1df23e66e63bae9d498ad2e37
Hanafe Mira <miraahanafee@gmail.com>
2024-12-26 11:52:23 +0530

    further styling node card header

--- app/workflow/_components/nodes/NodeComponent.tsx
+++ app/workflow/_components/nodes/NodeComponent.tsx
@@ -1,9 +1,16 @@
 import { NodeProps } from "@xyflow/react";
 import { memo } from "react";
 import NodeCard from "./NodeCard";
+import NodeHeader from "./NodeHeader";
+import { AppNodeData } from "@/types/appNode";
 
 const NodeComponent = memo((props : NodeProps) => {
-    return <NodeCard nodeId={props.id} isSelected={props.selected}>AppNode</NodeCard>
+    const nodeData = props.data as AppNodeData;
+    return (
+        <NodeCard nodeId={props.id} isSelected={!!props.selected}>
+            <NodeHeader taskType={nodeData.type} />
+        </NodeCard>
+    )
 });
 
 export default NodeComponent;
